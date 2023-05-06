48b75550871cb887ad95f7b2f789bfda10b6a414
Alex Rodriguess <alexmusicalsa@gmail.com>
2023-05-05 21:13:18 -0300

    segmento na primeira aula

---
+++ src/pages/basicos/primeiro.tsx
@@ -0,0 +1,13 @@
+import Primeiro from "@/src/components/basicos/Primeiro";
+
+export default function PrimeiraPagina() {
+    return (
+        <div>
+            <Primeiro />
+            <Primeiro />
+            <Primeiro />
+            <Primeiro />
+        </div>
+        
+    )
+}
