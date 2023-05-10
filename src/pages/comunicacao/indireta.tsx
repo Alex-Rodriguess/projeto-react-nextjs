2a581bf25934be7ba95f87e9d3984f949484f34a
Alex Rodriguess <alexmusicalsa@gmail.com>
2023-05-09 21:27:13 -0300

    continuando a aula 2

---
+++ src/pages/comunicacao/indireta.tsx
@@ -0,0 +1,15 @@
+import Contador from "@/src/components/comunicacao/indireta/Contador"
+
+export default function PaginaIndireta() {
+    return (
+        <div className="flex flex-col justify-center items-center">
+            <h1 className="text-5xl text-white font-black mb-10">Comunicação Indireta</h1>
+            <div className="gap-5 text-white">
+            <Contador />
+            <Contador />
+            <Contador />
+            </div>
+            
+        </div>
+    )
+}
