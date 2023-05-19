7f600d577c598e1af47772bab6f2d54d6bd4766d
Alex Rodriguess <alexmusicalsa@gmail.com>
2023-05-19 20:39:54 -0300

    aula2

---
+++ src/components/comunicacao/indireta/Botoes.tsx
@@ -0,0 +1,15 @@
+interface BotoesProps {
+   
+
+export default function Botoes (props: BotoesProps) {
+    return (
+        <div className="Botao">
+      <button>
+        Inc
+      </button>
+      <button>
+        Dec
+      </button>
+        </div>
+    )
+}
--- src/components/comunicacao/indireta/Contador.tsx
+++ src/components/comunicacao/indireta/Contador.tsx
@@ -1,7 +1,13 @@
+import Botoes from "./Botoes";
+import Display from "./display";
+
 export default function Contador() {
     return(
-       <div>
-        Contador
+       <div className={`
+       flex flex-col p-2 border border-zinc-400 rounded-lg
+       `}>
+        <Display valor={100} />
+        
        </div> 
     )
 }
