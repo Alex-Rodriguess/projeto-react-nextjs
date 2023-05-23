954f9095fdebb259ec4eceb93478f47b6c39a765
Alex Rodriguess <alexmusicalsa@gmail.com>
2023-05-23 20:18:58 -0300

    aes2

--- src/components/comunicacao/indireta/Contador.tsx
+++ src/components/comunicacao/indireta/Contador.tsx
@@ -1,12 +1,26 @@
+import { useState } from "react";
 import Botoes from "./Botoes";
 import Display from "./display";
 
 export default function Contador() {
+    const [num, setNum] = useState<number>(0)
+
+    function incrementar(valor: number) {
+        setNum(num + valor)
+    }
+
+    function decrementar(valor: number) {
+        setNum(num - valor)
+    }
+
     return(
        <div className={`
-       flex flex-col p-2 border border-zinc-400 rounded-lg
+       flex flex-col p-2 w-72 h-72
+       border border-zinc-400 
+       rounded-lg
        `}>
-        <Display valor={100} />
+        <Display valor={num} />
+        <Botoes inc={incrementar} dec={decrementar} />
         
        </div> 
     )
--- src/components/comunicacao/indireta/display.tsx
+++ src/components/comunicacao/indireta/display.tsx
@@ -4,7 +4,12 @@ interface DisplayProps {
 
 export default function Display (props: DisplayProps) {
     return (
-        <div className=" bg-zinc-800 text-5xl font-black rounded-md p-5">
+        <div className={`
+         flex justify-center items-center flex-1
+         bg-zinc-800 text-5xl 
+         font-black rounded-md p-5
+
+        `}>
             {props.valor}
         </div>
     )
